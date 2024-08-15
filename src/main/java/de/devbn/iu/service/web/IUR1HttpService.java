4bbb227b4001238405b5915f400ef47d1718d0a0
Brian Niebes <dev.BrianNiebes@gmail.com>
2024-08-16 01:38:33 +0200

    IUR1B-NONE - IMPL - Simple Healthcheck

--- src/main/java/de/devbn/iu/service/web/IUR1HttpService.java
+++ src/main/java/de/devbn/iu/service/web/IUR1HttpService.java
@@ -3,6 +3,8 @@ package de.devbn.iu.service.web;
 import de.devbn.iu.service.web.iur1.PlaylistHttpService;
 import io.helidon.webserver.http.HttpRules;
 import io.helidon.webserver.http.HttpService;
+import io.helidon.webserver.http.ServerRequest;
+import io.helidon.webserver.http.ServerResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,6 +16,11 @@ public class IUR1HttpService implements HttpService {
 
     @Override
     public void routing(final HttpRules httpRules) {
-        httpRules.register("/playlist", playlistHttpService);
+        httpRules.get("/health", this::health)
+                .register("/playlist", playlistHttpService);
+    }
+
+    private void health(final ServerRequest req, final ServerResponse res) {
+        res.send("UP");
     }
 }
